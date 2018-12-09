5e102db42e9e499e2c0139cefbba56b27764977f
Ameya Daddikar <ameyadaddikar@gmail.com>
2018-12-09 18:20:08 +0530

    Made a base template for all questions

--- app/routes.py
+++ app/routes.py
@@ -4,4 +4,7 @@ from flask import render_template
 @app.route('/', methods=['GET'])
 def index():
 	return render_template("base.html", title = "Home")
-	
+
+@app.route('/question_1', methods=['GET'])
+def question_1():
+	return render_template("question_1.html", title = "Trapping Rainwater")
